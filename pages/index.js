9a9dbf8a761e6f4157651ee9ce4d63afe366489d
mayankSH-addweb <mayank@addwebsolution.in>
2022-03-11 17:10:21 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Inventory Management system</title>
+      </Head>
+      <h1>Inventory Management System</h1>
+    </div>
+  )
+}
